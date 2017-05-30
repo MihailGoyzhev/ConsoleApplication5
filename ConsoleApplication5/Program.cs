fd21b7df06488f7aa9eeb95b25574041f3632a81
ГойжевМ <@403_7>
2017-05-30 20:43:34 +0600

    Add project files.

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Money
+{
+
+    public int currency;
+    public decimal Amount { get; set; }
+
+    public static Money operator ++ (Money f)
+    {
+        f.currency++;
+
+        return f;
+    }
+   
+}
+
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Money m = new Money();
+            m.currency = 100;
+
+            m++;
+
+            Console.WriteLine(m.currency);
+            
+        }
+    }
+}
